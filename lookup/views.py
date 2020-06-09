53f2d688d63b1b3f274d3e7deecca26c0a2fa265
Jakub Kaczmarek <feldmarschall.1941@gmail.com>
2020-06-09 18:31:49 +0200

    course finished

--- lookup/views.py
+++ lookup/views.py
@@ -5,7 +5,78 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-	return render(request, 'home.html', {})
+	import json
+	import requests
+
+	if request.method == 'POST':
+		zipcode = request.POST['zipcode']
+		api_request = requests.get('http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=' + zipcode + '&distance=1&API_KEY=DF9523C4-9021-4AD2-8C55-FB59FF492017')
+
+		try:
+			api = json.loads(api_request.content)
+		except Exception as e:
+			api = 'Error...'
+
+		if api[0]['Category']['Name'] == 'Good':
+			category_description = '(0 - 50) Air quality is satisfactory, and air pollution poses little or no risk.'
+			category_color = 'good'
+		elif api[0]['Category']['Name'] == 'Moderate':
+			category_description = '(51 - 100) Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.'
+			category_color = 'moderate'
+		elif api[0]['Category']['Name'] == 'Unhealthy for Sensitive Groups':
+			category_description = '(101 - 150) Members of sensitive groups may experience health effects. The general public is less likely to be affected.'
+			category_color = 'usg'
+		elif api[0]['Category']['Name'] == 'Unhealthy':
+			category_description = '(151 - 200) Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.'
+			category_color = 'unhealthy'
+		elif api[0]['Category']['Name'] == 'Very Unhealthy':
+			category_description = '(201 - 300) Health alert: The risk of health effects is increased for everyone.'
+			category_color = 'veryunhealthy'
+		elif api[0]['Category']['Name'] == 'Hazardous':
+			category_description = '(301 and higher) Health warning of emergency conditions: everyone is more likely to be affected.'
+			category_color = 'hazardous'
+
+		# http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=1&API_KEY=DF9523C4-9021-4AD2-8C55-FB59FF492017
+		return render(request, 'home.html', {
+			'api': api, 
+			'category_description': category_description, 
+			'category_color': category_color})
+
+	else:
+
+
+
+		api_request = requests.get('http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=1&API_KEY=DF9523C4-9021-4AD2-8C55-FB59FF492017')
+
+		try:
+			api = json.loads(api_request.content)
+		except Exception as e:
+			api = 'Error...'
+
+		if api[0]['Category']['Name'] == 'Good':
+			category_description = '(0 - 50) Air quality is satisfactory, and air pollution poses little or no risk.'
+			category_color = 'good'
+		elif api[0]['Category']['Name'] == 'Moderate':
+			category_description = '(51 - 100) Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.'
+			category_color = 'moderate'
+		elif api[0]['Category']['Name'] == 'Unhealthy for Sensitive Groups':
+			category_description = '(101 - 150) Members of sensitive groups may experience health effects. The general public is less likely to be affected.'
+			category_color = 'usg'
+		elif api[0]['Category']['Name'] == 'Unhealthy':
+			category_description = '(151 - 200) Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.'
+			category_color = 'unhealthy'
+		elif api[0]['Category']['Name'] == 'Very Unhealthy':
+			category_description = '(201 - 300) Health alert: The risk of health effects is increased for everyone.'
+			category_color = 'veryunhealthy'
+		elif api[0]['Category']['Name'] == 'Hazardous':
+			category_description = '(301 and higher) Health warning of emergency conditions: everyone is more likely to be affected.'
+			category_color = 'hazardous'
+
+	# http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=1&API_KEY=DF9523C4-9021-4AD2-8C55-FB59FF492017
+	return render(request, 'home.html', {
+		'api': api, 
+		'category_description': category_description, 
+		'category_color': category_color})
 
 def about(request):
 	return render(request, 'about.html', {})
